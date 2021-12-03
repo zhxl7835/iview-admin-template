c39770d907385e5ee93ca2bc98aa0c3817fdaa79
zhxl7835 <15216863219@163.com>
2021-12-04 03:12:22 +0800

    iview-admin-template第一次改造

---
+++ src/libs/axios.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import store from '@/store'
+// import { Spin } from 'iview'
+import { getToken } from '@/libs/util'
+const addErrorLog = errorInfo => {
+  const { statusText, status, request: { responseURL } } = errorInfo
+  let info = {
+    type: 'ajax',
+    code: status,
+    mes: statusText,
+    url: responseURL
+  }
+  if (!responseURL.includes('save_error_logger')) store.dispatch('addErrorLog', info)
+}
+
+class HttpRequest {
+  constructor (baseUrl = baseURL) {
+    this.baseUrl = baseUrl
+    this.queue = {}
+  }
+  getInsideConfig () {
+    const config = {
+      baseURL: this.baseUrl,
+      headers: {
+        //
+      }
+    }
+    return config
+  }
+  destroy (url) {
+    delete this.queue[url]
+    if (!Object.keys(this.queue).length) {
+      // Spin.hide()
+    }
+  }
+  interceptors (instance, url) {
+    // 请求拦截
+    instance.interceptors.request.use(config => {
+      // 添加全局的loading...
+      if (!Object.keys(this.queue).length) {
+        // Spin.show() // 不建议开启，因为界面不友好
+      }
+      this.queue[url] = true
+
+      //封装token
+      const token = getToken()
+      if (token){
+        config.headers['token'] = token
+      }
+      return config
+    }, error => {
+      return Promise.reject(error)
+    })
+    // 响应拦截
+    instance.interceptors.response.use(res => {
+      this.destroy(url)
+      const { data, status } = res
+      return { data, status }
+    }, error => {
+      this.destroy(url)
+      addErrorLog(error.response)
+      return Promise.reject(error)
+    })
+  }
+  request (options) {
+    const instance = axios.create()
+    options = Object.assign(this.getInsideConfig(), options)
+    this.interceptors(instance, options.url)
+    return instance(options)
+  }
+}
+export default HttpRequest
